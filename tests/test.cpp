4cba14171efda93232f617a7af45e563dd3a889a
Elegiacal <winner4166@gmail.com>
2018-12-05 09:44:40 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -2,12 +2,4 @@
 
 #include <gtest/gtest.h>
 
-boost::asio::ip::tcp::acceptor acceptor(service,
-    boost::asio::ip::tcp::endpoint
-    (boost::asio::ip::address::from_string
-    ("127.0.0.1"), 8001));
-auto user = std::make_shared<talk_to_client>();
-        acceptor.accept(user->sock());
-talk_to_client::users.push_back(user);
-
 #include <header.hpp>
